b239949dd4cb6db3af095f06c5b04adc86238ad0
kufayeka <yayankufa18@gmail.com>
2026-01-22 12:14:40 +0700

    update: set output:standalone

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     // Prevent Next dev from inferring the wrong root when multiple lockfiles exist.
     root: path.resolve(__dirname),
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
